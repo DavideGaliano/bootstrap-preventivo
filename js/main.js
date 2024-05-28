04eb567766a3f953f747ecbd546a8500ceb7272f
DavideGaliano <jconstantin@tiscali.it>
2024-05-28 10:38:13 +0200

    add: aggiunta validazione sul lavoro selezionato

--- js/main.js
+++ js/main.js
@@ -1,27 +1,59 @@
+
 // Funzione per calcolare il prezzo finale
 function calculatePrice() {
   // Definisci il numero di ore di lavoro per ogni progetto
   const hours = 10;
 
   // Ottieni il tipo di lavoro selezionato
-  const workType = document.getElementById("work").value;
+  const workTypeElement = document.getElementById("work");
+  const workType = workTypeElement.value;
 
   // Definisci il prezzo orario per ogni tipo di lavoro
   let hourlyRate;
   if (workType === "Backend Development") {
     hourlyRate = 20.5;
+    workTypeElement.classList.remove("is-invalid");
   } else if (workType === "Frontend Development") {
     hourlyRate = 15.3;
+    workTypeElement.classList.remove("is-invalid");
   } else if (workType === "Project Analysis") {
     hourlyRate = 33.6;
+    workTypeElement.classList.remove("is-invalid");
   } else {
-    // Se nessun tipo di lavoro è selezionato, mostra un messaggio di errore e interrompi
-    alert("Per favore seleziona un tipo di lavoro.");
+    // Se nessun tipo di lavoro è selezionato, mostra un messaggio di errore
+    workTypeElement.classList.add("is-invalid");
     return;
   }
 
   // Calcola il prezzo finale
-  const finalPrice = hours * hourlyRate;
+  let finalPrice = hours * hourlyRate;
+  const sconto = 25;
+
+  // Ottieni il codice promozionale inserito dall'utente
+  const promoCode = document.getElementById("codProm").value;
+
+  // Elenco dei codici promozionali validi
+  const validPromoCodes = [
+    "YHDNU32",
+    "JANJC63",
+    "PWKCN25",
+    "SJDPO96",
+    "POCIE24",
+  ];
+
+  // Verifica se il codice promozionale è valido
+  if (promoCode !== "" && validPromoCodes.includes(promoCode)) {
+    // Applica uno sconto del 25%
+    finalPrice = finalPrice - (finalPrice * sconto) / 100; // Sconto del 25%;
+    // Rimuovi lo stato di errore se presente
+    document.getElementById("codProm").classList.remove("is-invalid");
+  } else if (promoCode !== "") {
+    // Se il codice promozionale non è valido e non è vuoto, mostra un messaggio di errore
+    document.getElementById("codProm").classList.add("is-invalid");
+  } else {
+    // Rimuovi lo stato di errore se presente
+    document.getElementById("codProm").classList.remove("is-invalid");
+  }
 
   // Dividi il prezzo in parte intera e parte decimale
   const integerPart = Math.floor(finalPrice);
@@ -30,7 +62,7 @@ function calculatePrice() {
   // Aggiorna il contenuto dell'elemento con id "finalPrice" con gli stili richiesti
   document.getElementById(
     "finalPrice"
-  ).innerHTML = `<strong>€ ${integerPart}</strong><span class="text-secondary">,${decimalPart}</span>`;
+  ).innerHTML = `<strong>€ ${integerPart}</strong><span style="color: grey;">.${decimalPart}</span>`;
 }
 
 // Aggiungi un event listener per il click sul pulsante di submit
